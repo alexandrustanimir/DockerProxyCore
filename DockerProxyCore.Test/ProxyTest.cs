9ac7151778268503b38160193d062a4f3d0c3224
Alexandru Stanimir <alexandru.stanimir@nudgeit.com>
2018-03-24 13:49:22 +0200

    Added api test

--- DockerProxyCore.Test/ProxyTest.cs
+++ DockerProxyCore.Test/ProxyTest.cs
@@ -1,6 +1,5 @@
-using System.IO;
+using System;
 using System.Net.Http;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.TestHost;
 using Microsoft.Extensions.Configuration;
@@ -8,38 +7,34 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace DockerProxyCore.Test
 {
-    [TestClass]
-    public class ProxyTest
-    {
-	    private TestServer _server;
-	    private HttpClient _client;
+	[TestClass]
+	public class ProxyTest
+	{
+		private HttpClient _client;
 
 		[TestInitialize]
-	    public void Init()
-	    {
-			// Arrange
-		    var configuration = new ConfigurationBuilder()
-			    .AddJsonFile("appsettings.Development.json", optional: false)
-			    .Build();
-			_server = new TestServer(new WebHostBuilder()
-				.UseConfiguration(configuration)
-			    .UseStartup<Startup>());
-		    _client = _server.CreateClient();
+		public void Init()
+		{
+			// Arrange 
+			var configuration = new ConfigurationBuilder()
+				.AddJsonFile("appsettings.json", optional: false)
+				.Build();
+			_client = new HttpClient {BaseAddress = new Uri(configuration["BaseApiAddress"])};
 		}
 
 		[TestMethod]
-        public void CreatePaymentRequest_Request200Successfull()
+		public void CreatePaymentRequest_Request200Successfull()
 		{
 			//given an example of payment request
 			string content =
 				"{ \"payeePaymentReference\": \"0123456789\", \"callbackUrl\": \"https://example.com/api/swishcb/paymentrequests\", \"payerAlias\": \"4671234768\", \"payeeAlias\": \"1231181189\", \"amount\": \"100\", \"currency\": \"SEK\", \"message\": \"Kingston USB Flash Drive 8 GB\" }";
 
 			// when submiting the request
-			var response =_client.PostAsync(_server.BaseAddress + "api/Proxy/", new JsonContent(content)).Result;
+			var response = _client.PostAsync("api/Proxy/", new JsonContent(content)).Result;
 
 			// 
 			Assert.IsTrue(response.IsSuccessStatusCode);
 
 		}
-    }
+	}
 }
